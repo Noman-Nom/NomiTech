4a6b28dab4d70dfbd27c989b83bcf8ec785fadcf
Noman <noman5456318@gmail.com>
2023-10-22 16:16:29 +0500

    changes

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -1,33 +1,52 @@
-import React from 'react'
+import React from "react";
 
-import {AiFillGithub , AiFillFacebook, AiFillLinkedin,AiFillTwitterCircle, AiOutlineInstagram} from 'react-icons/ai'
-
+import {
+  AiFillGithub,
+  AiFillFacebook,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+  AiOutlineInstagram,
+} from "react-icons/ai";
 
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
-            <div>
-           <AiFillLinkedin/>
-           
-            </div>
-            <div>
-           <AiFillGithub/>
-
-            </div>
-            <div>
-           <AiFillFacebook/>
-
-            </div>
-            <div>
-           <AiFillTwitterCircle/>
-
-            </div>
-            <div>
-           <AiOutlineInstagram/>
-
-            </div>
+    <div className="app__social">
+      <div>
+        <a href="https://www.linkedin.com/in/muhammad-noman-770825277/">
+          <AiFillLinkedin />
+        </a>
+      </div>
+      <div>
+        <a href="https://github.com/Noman-Nom" target="_blank">
+          <AiFillGithub />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.facebook.com/muhammadnoman.awan.73?mibextid=ZbWKwL"
+          target="_blank"
+        >
+          <AiFillFacebook />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://x.com/NomiTechh?t=13PuVJbUrkGvwqPLn4Th7A&s=09"
+          target="_blank"
+        >
+          <AiFillTwitterCircle />
+        </a>
+      </div>
+      <div>
+      <a
+          href="https://x.com/NomiTechh?t=13PuVJbUrkGvwqPLn4Th7A&s=09"
+          target="_blank"
+        >
+          <AiOutlineInstagram />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
